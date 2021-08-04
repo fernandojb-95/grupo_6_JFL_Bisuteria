6b37d6847753aa51b67afa2c6948dc04aeedfaf4
fernandojb-95 <fernando.bjuarez95@gmail.com>
2021-08-04 18:44:59 -0500

    Se crea estructura de archivos Node + Express y se crea archivo retro

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname , './public')))
+app.listen(3000, () => {
+    console.log("Server running at port 3000")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+} )
