8643d9fc65206f56b4c74861a543e7ff0f9cf163
fernandojb-95 <fernando.bjuarez95@gmail.com>
2021-08-16 12:45:24 -0500

    Se agregan estructura HTML y estilos CSS a formularios login y register

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 } )
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+} )
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+} )
