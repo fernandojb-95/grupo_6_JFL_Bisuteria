110b15bf1788a9f4c24121a869320d76ddbf9654
lmoralesv96 <lmoralesv1100@alumno.ipn.mx>
2021-10-06 00:23:30 -0500

    Se realiza la configuracion de multer para el registro de nuevos usuarios y se inicia logica del metodo del controlador para almacenarlos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const usersPath = path.join(__dirname, '../data/users.json');
+
 const userController = {
     login: (req, res) => {
         res.render('./users/login');
     },
     register: (req, res) => {
         res.render('./users/register');
+    },
+    procesarRegistro: (req,res)=> {
+        //Lógica para almacenar usuarios nuevos
+        const products = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
+        const userName = req.body.user,
+        lastNameUser = req.body.lastname,
+        correo = req.body.email;
+
     }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const path = require('path');
+
+//Requerimos multer para traer archivos
+const multer = require('multer');
+
+//Configuramos destino y nombre de archivos
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) =>{
+        // let folder = path.join(__dirname, `../../public/img/profileImages`);
+        // callback(null,folder);
+    },
+    filename: (req, file, callback) => {
+        const imageName = '';
+        callback(null,imageName);
+    }
+})
+const fileUpload = multer({storage: multerDiskStorage});
 
 /*----Rutas paravista de formulario de login----*/
 router.get('/login', userController.login);
 
-/*----Rutas paravista de formulario de registro----*/
+/*----Rutas para vista de formulario de registro----*/
 router.get('/register', userController.register);
+router.post('/', fileUpload.single('imagenUsuario'), userController.procesarRegistro);
 
 module.exports = router;
