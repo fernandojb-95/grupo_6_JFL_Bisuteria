673012c97967122cb2a5278366b6eb54dcf639d7
fernandojb-95 <fernando.bjuarez95@gmail.com>
2021-11-16 19:26:07 -0600

    Se actualiza el diagrama de creacion y poblado de datos

--- src/database/Script de creacion JFL_Database.sql
+++ src/database/Script de creacion JFL_Database.sql
@@ -5,12 +5,12 @@ USE jfl_database;
 drop table if exists `users`;
 create table users(
 `id` int(10) unsigned unique auto_increment not null,
-`first_name` varchar(50) not null,
-`last_name` varchar(50) not null,
-`email` varchar(50) not null,
-`password` varchar(50) not null,
+`first_name` varchar(100) not null,
+`last_name` varchar(100) not null,
+`email` varchar(100) not null,
+`password` varchar(200) not null,
 `isAdmin` tinyint not null,
-`image` varchar(50) null default 'default-user.png',
+`image` varchar(100) null default 'default-user.png',
 primary key (`id`));
 
 drop table if exists `categories`;
@@ -29,14 +29,15 @@ drop table if exists `products`;
 create table products(
 `id` int(10) unsigned unique auto_increment not null,
 `name` varchar(100) not null,
-`description` varchar(100) not null,
+`description` varchar(300) not null,
 `price` decimal(7,2) not null,
 `discount` decimal(4,2) not null,
 `quantity_S` int(10) unsigned null default 0,
 `quantity_M` int(10) unsigned null default 0,
 `quantity_L` int(10) unsigned null default 0,
-`image_1` varchar(50) not null,
-`image_2` varchar(50) not null,
+`image_1` varchar(100) NULL DEFAULT 'default-image.svg',
+`image_2` varchar(100) NULL DEFAULT 'default-image.svg',
+`sold` int(10) unsigned null default 0,
 `category_id` int(10) unsigned not null,
 `material_id` int(10) unsigned not null,
 primary key (`id`),
---
+++ src/database/data.sql
@@ -0,0 +1,147 @@
+insert into `categories` values (1, 'anillos'), (2, 'brazaletes'), (3,'collares');
+insert into `materials` values (1, 'acero'), (2, 'oro'), (3,'plata');
+
+insert into `products` values (1,'Ariane',
+'Este anillo posee un dise?o sencillo pero elegante. Ariane tiene la ventaja de que se lleva bien con cualquier otra joya. Su fineza permite que se combine con lo que sea que te pongas y te permite verte muy estilizado.', 
+349.00, 20, 20, 20, 20, 'img-anillos-84087-acero.png', 'img-anillos-84094-acero.png', 5, 1, 1);
+
+insert into `products` values (2,'Katia',
+'Anillo de acero con un estilo que aparenta desgaste, para todos aquellos que buscan un estilo fresco y m?s callejero. Va perfecto para usar con jeans desgastados.', 
+349.00, 20, 20, 20, 20, 'img-anillos-02023-acero.png', 'img-anillos-02028-acero.png', 8, 1, 1);
+
+insert into `products` values (3,'Soraya',
+'Soraya es un anillo de acero con tinte negro, al centro tiene unos incre?bles detalles a los que llamamos escamas de acero que le dan una esencia ?nica. Es un anillo muy moderno y discreto pero por su estilo particular no logra pasar desapercibido.', 
+349.00, 21, 20, 20, 20, 'img-anillos-23356-acero.png', 'img-anillos-23377-acero.png', 2, 1, 1);
+
+insert into `products` values (4,'Amelia',
+'Amelia es un anillo que nos recuerda las ?pocas de los dragones, por su acabado simula la cola enroscada de un drag?n. Este anillo es muy combinable y va perfecto con cualquier estilo', 
+499.00, 19, 20, 20, 20, 'img-anillos-77314-plata.png', 'img-anillos-77345-plata.png', 10, 1, 3);
+
+insert into `products` values (5,'Maria',
+'Un anillo muy particular ya que contiene un peque?o acabado de oro al centro aplastado por dos piezas de plata, lo cual lo hace muy llamativo y estilizado. Le aporta mucha formalidad a quien lo usa.', 
+499.00, 10, 20, 20, 20, 'img-anillos-20149-plata.png', 'img-anillos-20152-plata.png', 1, 1, 3);
+
+insert into `products` values (6,'Ainara',
+'Ainara es m?s que un anillo, es la combinaci?n de 3 anillos enlazados para darnos este dise?o tan especial, su material y color permite que se combine con otros anillos dando un estilo muy moderno.', 
+499.00, 25, 20, 20, 20, 'img-anillos-50333-plata.png', 'img-anillos-50359-plata.png', 1, 1, 3);
+
+insert into `products` values (7,'Sara',
+'Anillo de oro con peque?os acabados alrededor de todo el metal. Sara es un anillo muy c?modo y sutil perfecto para aquellos que les gusta tener un adorno llamativo y discreto, una dualidad perfecta.', 
+559.00, 0, 20, 20, 20, 'img-anillos-92639-oro.png', 'img-anillos-92667-oro.png', 5, 1, 2);
+
+insert into `products` values (8,'Montse',
+'Montse  es un anillo sencillo pero combinable, te puede acompa?ar con el estilo que sea, ya sea formal o casual. Su dise?o liso te permite combinarlo con cualquier outfit y con otros accesorios.', 
+559.00, 5, 20, 20, 20, 'img-anillos-35132-oro.png', 'img-anillos-35152-oro.png', 3, 1, 2);
+
+insert into `products` values (9,'Juliana',
+'El dise?o de Juliana al tener acabados cuadrados, nos da un estilo de fortaleza y poder, dando la impresi?n de ser de la realeza. Con este anillo tus outfits transmitir?n tintes de empoderamiento.', 
+559.00, 5, 20, 20, 20, 'img-anillos-68114-oro.png', 'img-anillos-68149-oro.png', 8, 1, 2);
+
+insert into `products` values (10,'Hanane',
+'Este anillo oculta sus detalles de oro detr?s de unos acabados de piedra volc?nica, dando un estilo misterioso al anillo. Con este anillo se puede usar de forma m?s com?n ya que al ocultar el oro no es muy llamativo.', 
+559.00, 10, 20, 20, 20, 'img-anillos-00843-oro.png', 'img-anillos-00880-oro.png', 8, 1, 2);
+
+insert into `products` values (11,'Sandra',
+'Para todos aquellos que les gusta que los voltean a ver, Sandra es todo menos sutil, es un brazalete muy llamativo con acabos que le dan gran clase. Perfecto para llevar con outfits formales y robar las miradas.', 
+449.00, 0, 20, 20, 20, 'img-brazaletes-90779-oro.png', 'img-brazaletes-90796-oro.png', 15, 2, 2);
+
+insert into `products` values (12,'Andrea',
+'Una cadena que representa la frase menos es m?s, con su dise?o sencillo es f?cil de combinar y se puede llevar a toda ocasi?n formal o casual. Perfecto para utilizar con otro brazalete y darle una gran mejora a tu estilo.', 
+449.00, 0, 20, 20, 20, 'img-brazaletes-20435-oro.png', 'img-brazaletes-20459-oro.png', 3, 2, 2);
+
+insert into `products` values (13,'Cecilia',
+'Cecilia es uno de nuestros brazaletes favoritos, con un dise?o que combina oro y acero negro, da la impresi?n de ser un brazalete salido de un volc?n. Es una pulsera que puedes llevar a todos lados y que te permitir? robar halagos.', 
+449.00, 10, 20, 20, 20, 'img-brazaletes-45883-oro.png', 'img-brazaletes-45904-oro.png', 7, 2, 2);
+
+insert into `products` values (14,'Maitane',
+'Maitane es una pulsera de dise?o sencillo pero muy llamativo, las lienas que se enroscan por toda la pulsera le da unos tonos extravagantes que permiten usarse para toda ocasi?n.', 
+399.00, 10, 20, 20, 20, 'img-brazaletes-76007-plata.png', 'img-brazaletes-76055-plata.png', 11, 2, 3);
+
+insert into `products` values (15,'Emma',
+'La cl?sica cadena de mu?eca, para todos los amantes de los estilos cl?sicos, este brazalete es el accesorio perfecto para las personas que aman el estilo informal. Tiene como plus que da una sensaci?n de chico malo.', 
+399.00, 9, 20, 20, 20, 'img-brazaletes-06398-plata.png', 'img-brazaletes-06428-plata.png', 10, 2, 3);
+
+insert into `products` values (16,'Vanesa',
+'Vanesa es un brazalete sumamente elegante, con su dise?o ?nico permite darle mucho porte y clase a quien lo usa. Con este brazalete vas a resaltar en cualquier evento formal. Perfecto para combinar con brazaletes de oro.', 
+399.00, 5, 20, 20, 20, 'img-brazaletes-36316-plata.png', 'img-brazaletes-36368-plata.png', 1, 2, 3);
+
+insert into `products` values (17,'Ariana',
+'Un brazalete sencillo pero elegante, con detalles de eslabones gruesos que le dan un toque rudo a tu outfit. F?cil de combinar, debido a que su color va bien con lo que te pongas.', 
+379.00, 0, 20, 20, 20, 'img-brazaletes-69350-plata.png', 'img-brazaletes-69360-plata.png', 11, 2, 3);
+
+insert into `products` values (18,'Victoria',
+'Victoria es un brazalete con eslabones gruesos pero con acabados cuadrados, con una bonita placa dorada. Este dise?o otorga una gran clase ya que la combinaci?n de materiales lo hace destacar sobre cualquier mirada.', 
+379.00, 10, 20, 20, 20, 'img-brazaletes-99398-acero.png', 'img-brazaletes-99420-acero.png', 10, 2, 1);
+
+insert into `products` values (19,'Minerva',
+'Minerva es un brazalete que combina dos materiales, tela y acero. La tela color negro le da un estilo elegante y los detalles de acero permiten darle a Minerva un grane estilo y que lo hace resaltar. Pensado en todos los que buscan portar su religi?n con estilo.',
+379.00, 5, 20, 20, 20, 'img-brazaletes-32496-acero.png', 'img-brazaletes-32505-acero.png', 18, 2, 1);
+
+insert into `products` values (20,'Isabella',
+'Isabella representa a la perfecci?n lo que es la elegancia, con peque?os detalles se convierte en un brazalete de gran clase que le da el toque necesario para tu etilo formal.',
+379.00, 0, 20, 20, 20, 'img-brazaletes-59323-acero.png', 'img-brazaletes-59393-acero.png', 8, 2, 1);
+
+insert into `products` values (21,'Teodora',
+'Esta cadena de plata tiene un estilo ?nico, al cambiar de patrones evita verse como todas las dem?s. Se ve muy bien por si sola pero tambi?n es perfecta para llevar un dije que le d? un toque m?s personal.',
+699.00, 10, 20, 20, 20, 'img-collares-71369-plata.png', 'img-collares-71378-plata.png', 10, 3, 3);
+
+insert into `products` values (22,'Candela',
+'Candela es la cl?sica cadena de plata, para llevarla con todos los outfits, es la que nunca pasa de moda y siempre es bueno tenerla. Es tu fiel acompa?ante para darle algo de estilo a tu look.',
+699.00, 5, 20, 20, 20, 'img-collares-44685-plata.png', 'img-collares-44695-plata.png', 5, 3, 3);
+
+insert into `products` values (23,'Ainhoa',
+'La cadena Ainhoa es un collar que se hace notar, de tonos muy brillantes y de un ancho amplio. Esta luce perfecto con los estilos modernos y por si sola te har? ver incre?ble, sin necesidad de dijes u otras cadenas.',
+699.00, 0, 20, 20, 20, 'img-collares-20477-plata.png', 'img-collares-20498-plata.png', 2, 3, 3);
+
+insert into `products` values (24,'Aerola',
+'Aerola es la cadena para todos aquellos que desean portar un look m?s rudo, es un collar cl?sico, muy utilizado por motociclistas y todos aquellos que buscan mostrar su personalidad intr?pida.',
+699.00, 10, 20, 20, 20, 'img-collares-91024-plata.png', 'img-collares-91050-plata.png', 20, 3, 3);
+
+insert into `products` values (25,'Roc?o',
+'Roc?o es un collar de oro, con acabados muy bonitos que dan la sensaci?n de ser listones los que unen los eslabones. Es una cadena de dise?o ?nico para todos aquellos que buscan un estilo formal.',
+729.00, 0, 20, 20, 20, 'img-collares-34558-oro.png', 'img-collares-34591-oro.png', 10, 3, 2);
+
+insert into `products` values (26,'Uxia',
+'Este collar que lleva un anillo como colgante, es uno de nuestros favoritos ya que da mucho porte y le da un estilo muy elegante a quien lo usa. Al ser un collar muy brillante nunca te va a permitir pasar desapercibido, ser?s el centro de atenci?n.',
+729.00, 10, 20, 20, 20, 'img-collares-94749-oro.png', 'img-collares-94769-oro.png', 1, 3, 2);
+
+insert into `products` values (27,'Helena',
+'Es una cadena que puede parecer sencilla pero sus acabados que evocan las escamas de una serpiente te da un estilo que resalta y permite darte un look m?s extravagante. Es un collar que solo algunos se atreven a usar.',
+729.00, 0, 20, 20, 20, 'img-collares-47862-oro.png', 'img-collares-47889-oro.png', 5, 3, 2);
+
+insert into `products` values (28,'Micaela',
+'Micaela es un collar muy parecido a Uxia, pero es para personas que buscan algo m?s discreto y  m?s variedad para combinar. Su color negro permite que se combine con m?s atuendos  y sin importar si es formal o casual',
+559.00, 10, 20, 20, 20, 'img-collares-00234-acero.png', 'img-collares-00236-acero.png', 15, 3, 1);
+
+insert into `products` values (29,'Tania',
+'Una cadena de acero negro que se puede combinar con cualquier otro collar de nuestro inventario. Da un estilo misterioso con enfoque en outfits casuales.',
+559.00, 5, 20, 20, 20, 'img-collares-98170-acero.png', 'img-collares-98202-acero.png', 3, 3, 1);
+
+insert into `products` values (30,'Nerea',
+'Nerea es una cadena que mantiene el color plateado del acero, con muy bonitos acabados y con el detalle de llevar una cruz. El detalle de la cruz esta pensado para todas las personas que buscan llevar con gran estilo su religi?n.',
+559.00, 0, 20, 20, 20, 'img-collares-34710-acero.png', 'img-collares-34735-acero.png', 13, 3, 1);
+
+insert into `users` (id, first_name, last_name, email, password, isAdmin) values (1, 'Fernando', 'Juarez', 
+'fernandojb@jflbisuteria.com.mx', '$2b$10$GeKT4nsQhCcRjky5jRQ0iehoY.8CfYsGhEQVelVV79k6Q425hpV.2', 1);
+
+insert into `users` (first_name, last_name, email, password, isAdmin) values ('Daniela', 'Alvarez', 
+'daniela@gmail.com', '$2b$10$CpohL21SSbfu5vSMZeUYGe9kfq49syipZO7h.ZJiGpCezOAr4HB9W', 0);
+
+insert into `users` (first_name, last_name, email, password, isAdmin, image) values ('Gerardo', 'Landaverde', 
+'jerrylanda@jflbisuteria.com.mx', '$2b$10$PpnyU0dXViXDDVB4vrQLfOvU4tEAGjQtrRfaqQWluOKTEQwZkefra', 1, 
+'img-gerardo-38435.png');
+
+insert into `users` (first_name, last_name, email, password, isAdmin, image) values ('Bruce', 'Wayne', 
+'brucewayne@batman.com', '$2b$10$SDt8lqxZDVb2yvhJjokCse/MI4df1wk7.NtF24vTJGyjH.bp9bnzW', 0, 'img-bruce-01742.jpg');
+
+
+
+select products.name, products.description, materials.name as material, categories.name as category from products
+inner join materials on materials.id = products.material_id
+inner join categories on categories.id = products.category_id
+order by products.id;
+
+select * from users;
+
+
+
